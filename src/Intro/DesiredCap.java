2339ad218854dd76bc93d778660be2b3c376e4de
alohageck0 <royalfiish@gmail.com>
2016-04-08 00:19:19 -0700

    home 04/08/16

--- src/Intro/DesiredCap.java
+++ src/Intro/DesiredCap.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 
 public class DesiredCap {
@@ -28,7 +27,7 @@ public class DesiredCap {
       cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 100);
       cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
       AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-      driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+//      driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
       driver.findElementById("com.bt.bms:id/btnLogin").click();
    }
 }
