76c426b0ccdd06f2bf557c28086db346e2092e73
alohageck0 <royalfiish@gmail.com>
2016-04-08 00:09:53 -0700

    home 04/08/16

--- src/Intro/DesiredCap.java
+++ src/Intro/DesiredCap.java
@@ -3,27 +3,32 @@ package Intro;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
+import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 
 public class DesiredCap {
-   public static void main(String[] args) throws MalformedURLException {
-      test();
-   }
+//   public static void main(String[] args) throws MalformedURLException {
+//      test();
+//   }
 
-   public static void test() throws MalformedURLException {
+   @Test
+   public void test() throws MalformedURLException, InterruptedException {
       File appDir = new File("src");
       File app = new File(appDir, "BookMyShow.apk");
 
       DesiredCapabilities cap = new DesiredCapabilities();
       cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
-      cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+      cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Moto");
+      cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 100);
       cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
       AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-
+      driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+      driver.findElementById("com.bt.bms:id/btnLogin").click();
    }
 }
