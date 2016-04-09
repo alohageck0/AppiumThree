f06b2785d13639482e1c1fdc0b64f77a9ac667e2
alohageck0 <royalfiish@gmail.com>
2016-04-09 16:52:23 -0700

    home 04/09/16 UI

--- src/Intro/Program2.java
+++ src/Intro/Program2.java
@@ -2,9 +2,20 @@ package Intro;
 
 import com.github.genium_framework.appium.support.server.AppiumServer;
 import com.github.genium_framework.server.ServerArguments;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 public class Program2 {
    private ServerArguments serverArguments = new ServerArguments();
@@ -18,6 +29,7 @@ public class Program2 {
       serverArguments.setArgument("--no-reset", true);
 
       serverArguments.setArgument("--local-timezone", true);
+      serverArguments.setArgument("--device-ready-timeout", "10");
       appiumServer = new AppiumServer(serverArguments);
       appiumServer.startServer();
    }
@@ -28,7 +40,20 @@ public class Program2 {
    }
 
    @Test
-   public void test() {
+   public void test() throws MalformedURLException {
+      File appDir = new File("src");
+      File app = new File(appDir, "ApiDemos-debug.apk");
+      DesiredCapabilities cap = new DesiredCapabilities();
+      cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+      cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Moto");
+      cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20);
+      cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+      AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+//      driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
+      driver.scrollTo("Preference").click();
+      List<WebElement> a = driver.findElements(By.id("android:id/text1"));
+      System.out.println(a.get(8).getText());
    }
+
 }
