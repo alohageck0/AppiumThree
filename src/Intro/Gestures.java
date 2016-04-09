7d8526cd6c9089b014d8e0722f5357949b2b121f
alohageck0 <royalfiish@gmail.com>
2016-04-09 16:56:11 -0700

    home 04/09/16

---
+++ src/Intro/Gestures.java
@@ -0,0 +1,57 @@
+package Intro;
+
+import com.github.genium_framework.appium.support.server.AppiumServer;
+import com.github.genium_framework.server.ServerArguments;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Gestures {
+   private ServerArguments serverArguments = new ServerArguments();
+   private AppiumServer _appiumServer;
+
+   @Before
+   public void startUp() {
+      serverArguments = new ServerArguments();
+      serverArguments.setArgument("--address", "127.0.0.1");
+
+      serverArguments.setArgument("--no-reset", true);
+
+      serverArguments.setArgument("--local-timezone", true);
+      serverArguments.setArgument("--device-ready-timeout", "10");
+
+      _appiumServer = new AppiumServer(serverArguments);
+      _appiumServer.startServer();
+   }
+
+   @After
+   public void cleanUp() {
+      _appiumServer.stopServer();
+   }
+
+   @Test
+   public void test() throws MalformedURLException, InterruptedException {
+      File appDir = new File("src");
+      File app = new File(appDir, "BookMyShow.apk");
+      DesiredCapabilities cap = new DesiredCapabilities();
+      cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+      cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Moto");
+      cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 200);
+      cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+      AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
+//      driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+
+
+   }
+}
