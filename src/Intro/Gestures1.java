1fd71ff83279aa7c944e494775c39d8cbc578957
alohageck0 <royalfiish@gmail.com>
2016-04-09 17:57:00 -0700

    home 04/09/16

---
+++ src/Intro/Gestures1.java
@@ -0,0 +1,62 @@
+package Intro;
+
+import com.github.genium_framework.appium.support.server.AppiumServer;
+import com.github.genium_framework.server.ServerArguments;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Gestures1 {
+   private ServerArguments serverArguments = new ServerArguments();
+   private AppiumServer appiumServer;
+
+   @Before
+   public void startUp() {
+      serverArguments = new ServerArguments();
+      serverArguments.setArgument("--address", "127.0.0.1");
+
+      serverArguments.setArgument("--no-reset", true);
+
+      serverArguments.setArgument("--local-timezone", true);
+      serverArguments.setArgument("--device-ready-timeout", "10");
+      appiumServer = new AppiumServer(serverArguments);
+      appiumServer.startServer();
+   }
+
+   @After
+   public void cleanUp() {
+      appiumServer.stopServer();
+   }
+
+   @Test
+   public void test() throws MalformedURLException {
+      File appDir = new File("src");
+      File app = new File(appDir, "ApiDemos-debug.apk");
+      DesiredCapabilities cap = new DesiredCapabilities();
+      cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+      cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Moto");
+      cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20);
+      cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+      AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+//      driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+      System.out.println(driver.isLocked());
+      System.out.println(driver.getContext());
+
+//      driver.installApp("");
+
+
+      driver.closeApp();
+//      driver.currentActivity();
+//      driver.context()
+   }
+
+}
