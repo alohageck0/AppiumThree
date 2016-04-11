7f935a31d9fc127c1b2ff2911a4dfbecb2e2a77c
alohageck0 <royalfiish@gmail.com>
2016-04-10 17:53:28 -0700

    home 04/10/16

---
+++ src/Intro/PackageActivity.java
@@ -0,0 +1,56 @@
+package Intro;
+
+import com.github.genium_framework.appium.support.server.AppiumServer;
+import com.github.genium_framework.server.ServerArguments;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.sun.tools.doclint.Entity.cap;
+
+public class PackageActivity {
+   private ServerArguments serverArguments = new ServerArguments();
+   private AppiumServer appiumServer;
+
+   @Before
+   public void startUp() {
+      serverArguments = new ServerArguments();
+      serverArguments.setArgument("--address", "127.0.0.1");
+
+      serverArguments.setArgument("--no-reset", true);
+
+      serverArguments.setArgument("--local-timezone", true);
+      serverArguments.setArgument("--device-ready-timeout", "10");
+      appiumServer = new AppiumServer(serverArguments);
+      appiumServer.startServer();
+   }
+
+   @After
+   public void cleanUp() {
+      appiumServer.stopServer();
+   }
+
+   @Test
+   public void test() throws MalformedURLException {
+      File appDir = new File("src");
+
+
+      DesiredCapabilities cap = new DesiredCapabilities();
+      cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+      cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android device");
+      cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20);
+
+      AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
+
+   }
+
+}
