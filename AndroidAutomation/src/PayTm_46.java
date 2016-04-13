5f14de81e3ddc941a80d9316ad62a787686a8e53
alohageck0 <royalfiish@gmail.com>
2016-04-12 20:47:27 -0700

    home 04/12/16

--- AndroidAutomation/src/PayTm_46.java
+++ AndroidAutomation/src/PayTm_46.java
@@ -2,19 +2,16 @@ import com.github.genium_framework.appium.support.server.AppiumServer;
 import com.github.genium_framework.server.ServerArguments;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 public class PayTm_46 {
    private ServerArguments serverArguments = new ServerArguments();
@@ -22,11 +19,8 @@ public class PayTm_46 {
 
    @Before
    public void startUp() {
-      serverArguments = new ServerArguments();
       serverArguments.setArgument("--address", "127.0.0.1");
-
-      serverArguments.setArgument("--no-reset", true);
-
+//      serverArguments.setArgument("--no-reset", true);
       serverArguments.setArgument("--local-timezone", true);
       serverArguments.setArgument("--device-ready-timeout", "100");
       appiumServer = new AppiumServer(serverArguments);
@@ -51,32 +45,26 @@ public class PayTm_46 {
       cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 //      cap.setCapability("appPackage", "com.raaga.android");
 //      cap.setCapability("appActivity", "com.raaga.android.SplashScreen");
-      cap.setCapability(MobileCapabilityType.NO_RESET, true);
+//      cap.setCapability(MobileCapabilityType.NO_RESET, true);
       cap.setCapability("deviceReadyTimeout", 40);
 
       AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-      Thread.sleep(8000);
+      Thread.sleep(7000);
 
-      driver.swipe(5, 500, 250, 500, 1000);
-      TouchAction touchAction = new TouchAction(driver);
-      touchAction.tap(driver.findElementById("com.raaga.android:id/menu_lanuguages")).perform();
-      List<WebElement> languages = driver.findElementsByClassName("android.widget.TextView");
-      int countLanguages = 0;
-      for (WebElement lang : languages) {
-         if (!lang.getText().equals("Languages")) {
-            countLanguages++;
-         }
+      try {
+         driver.swipe(50, 100, 60, 120, 1000);
+      } catch (Exception e) {
+         e.printStackTrace();
       }
-      System.out.println(countLanguages);
+      driver.findElementById("net.one97.paytm:id/edit_no").sendKeys("2768539");
 
-      touchAction.tap(driver.findElementByAndroidUIAutomator("new UiSelector().text(\"Cancel\")")).perform();
-      Thread.sleep(3000);
-
-      driver.swipe(5, 500, 250, 500, 1000);
-      driver.pressKeyCode(AndroidKeyCode.BACK);
-      Thread.sleep(3000);
-
-      driver.pressKeyCode(AndroidKeyCode.HOME);
+      TouchAction touchAction = new TouchAction(driver);
+      touchAction.tap(driver.findElementById("net.one97.paytm:id/lyt_mob_op")).perform();
+      driver.scrollTo("Vodafone");
+      touchAction.tap(driver.findElementByAndroidUIAutomator("new UiSelector().text(\"Vodafone\")")).perform();
+//      driver.scrollTo("Mumbai");
+      driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Mumbai\").instance(0))");
+      touchAction.tap(driver.findElementByAndroidUIAutomator("new UiSelector().text(\"Mumbai\")")).perform();
 
    }
 }
