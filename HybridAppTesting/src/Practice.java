eaa52209d37ffcc353a0b1ba99c1e6a901e4c5ed
alohageck0 <royalfiish@gmail.com>
2016-04-14 23:41:06 -0700

    home 04/14/16

--- HybridAppTesting/src/Practice.java
+++ HybridAppTesting/src/Practice.java
@@ -1,3 +1,18 @@
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
 public class Practice {
+
+   DesiredCapabilities capabilities = new DesiredCapabilities();
+
+   @Test
+   public void Test() {
+      capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+      capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android device");
+      capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20);
+
+   }
 
 }
