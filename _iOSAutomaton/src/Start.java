6858ad1560b99f7b777f199f7350eb494e0d6e0b
alohageck0 <royalfiish@gmail.com>
2016-04-17 13:46:42 -0700

    home 04/17/16

--- _iOSAutomaton/src/Start.java
+++ _iOSAutomaton/src/Start.java
@@ -1,8 +1,9 @@
 import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Start {
    @Test
    public void Test() {
-
+      DesiredCapabilities capabilities;
    }
 }
---
+++ untitled1/src/Test11.java
@@ -0,0 +1,7 @@
+import org.junit.Test;
+
+public class Test11 {
+   @Test
+   public void Test(){
+   }
+}
