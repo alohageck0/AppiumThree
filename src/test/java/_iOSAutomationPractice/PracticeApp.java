ed2a5499d14a7736803a25f3dd2c7d9bf057f2d7
alohageck0 <royalfiish@gmail.com>
2016-04-17 23:28:34 -0700

    home 04/17/16

--- src/test/java/_iOSAutomationPractice/PracticeApp.java
+++ src/test/java/_iOSAutomationPractice/PracticeApp.java
@@ -1,14 +1,21 @@
 package _iOSAutomationPractice;
 
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+
 public class PracticeApp {
    @Before
    public void setUp() {
+      File app = new File("/Users/royalfiish/IdeaProjects/UdemyAppium/src/test/java/_iOSAutomationPractice/UICatalog.app");
       DesiredCapabilities capabilities = new DesiredCapabilities();
-
+      capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+      capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.3");
+      capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPad 2");
+      capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
    }
 
    @Test
