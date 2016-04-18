837e5348ef2490faabffa2b645ef0e5d23294a90
alohageck0 <royalfiish@gmail.com>
2016-04-17 23:16:43 -0700

    home 04/17/16

---
+++ src/test/java/_iOSAutomationPractice/PracticeApp.java
@@ -0,0 +1,19 @@
+package _iOSAutomationPractice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class PracticeApp {
+   @Before
+   public void setUp() {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+
+   }
+
+   @Test
+   public void Test() {
+
+
+   }
+}
