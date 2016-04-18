952dfe1bfe20df778f6be46fc8f1334939c0b9a1
alohageck0 <royalfiish@gmail.com>
2016-04-17 23:52:05 -0700

    home 04/17/16

--- src/test/java/_iOSAutomationPractice/PracticeApp.java
+++ src/test/java/_iOSAutomationPractice/PracticeApp.java
@@ -2,6 +2,7 @@ package _iOSAutomationPractice;
 
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,18 +14,25 @@ import java.net.URL;
 public class PracticeApp {
    DesiredCapabilities capabilities = new DesiredCapabilities();
    File app = new File("/Users/royalfiish/IdeaProjects/UdemyAppium/src/test/java/_iOSAutomationPractice/UICatalog.app");
+   IOSDriver driver;
 
    @Before
-   public void setUp() {
+   public void setUp() throws MalformedURLException {
       capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
       capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.3");
       capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 4s");
       capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
+      driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+   }
+
+   @After
+   public void cleanUp() {
+      driver.close();
    }
 
    @Test
    public void Test() throws MalformedURLException {
-      IOSDriver driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
 
    }
