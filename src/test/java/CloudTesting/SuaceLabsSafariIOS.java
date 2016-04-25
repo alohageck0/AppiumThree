cae1be9c62559782708add93c4fd5674e6f04cd4
alohageck0 <royalfiish@gmail.com>
2016-04-24 17:43:39 -0700

    home 04/24/16

--- src/test/java/CloudTesting/SuaceLabsSafariIOS.java
+++ src/test/java/CloudTesting/SuaceLabsSafariIOS.java
@@ -1,24 +1,38 @@
 package CloudTesting;
 
+import io.appium.java_client.ios.IOSDriver;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class SuaceLabsSafariIOS {
-   DesiredCapabilities capabilities = DesiredCapabilities.iphone();
+   private DesiredCapabilities capabilities = DesiredCapabilities.iphone();
+   private IOSDriver driver;
+   private String url = new Scanner(new File("/Users/royalfiish/IdeaProjects/UdemyAppium/src/test/java/CloudTesting/url")).nextLine();
+
+   public SuaceLabsSafariIOS() throws FileNotFoundException {
+   }
 
    @Before
-   public void setUp() {
+   public void setUp() throws MalformedURLException {
       capabilities.setCapability("platformName", "iOS");
-      capabilities.setCapability("platformVersion", "7.1");
-      capabilities.setCapability("appiumVersion", "1.3.4");
+      capabilities.setCapability("platformVersion", "9.1");
+      capabilities.setCapability("appiumVersion", "1.5.1");
       capabilities.setCapability("deviceName", "iPhone Simulator");
-      capabilities.setCapability("browserName", "Safari");
+      capabilities.setCapability("browserName", "safari");
 
+      driver = new IOSDriver(new URL(url), capabilities);
    }
 
    @Test
-   public void test() {
+   public void test() throws InterruptedException {
+      driver.get("http://udemy.com");
 
    }
 
