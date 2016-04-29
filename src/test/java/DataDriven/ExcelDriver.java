66c36901b9477a5a2ce29b7102a8427147e544b4
alohageck0 <royalfiish@gmail.com>
2016-04-29 07:17:51 -0700

    home 04/29/16

--- src/test/java/DataDriven/ExcelDriver.java
+++ src/test/java/DataDriven/ExcelDriver.java
@@ -21,12 +21,23 @@ public class ExcelDriver {
       String val = getCellData(2, 2);
       System.out.println(val);
 
+      setCellData(2,2, "Hello new data");
+      val = getCellData(2, 2);
+      System.out.println(val);
+
    }
 
    public static String getCellData(int rowNum, int colNum) {
       sheet = workbook.getSheet("script");
       row = sheet.getRow(rowNum);
       cell = row.getCell(colNum);
+      return cell.getStringCellValue();
+   }
+   public static String setCellData(int rowNum, int colNum, String text) {
+      sheet = workbook.getSheet("script");
+      row = sheet.getRow(rowNum);
+      cell = row.getCell(colNum);
+      cell.setCellValue(text);
       return cell.getStringCellValue();
    }
 }
