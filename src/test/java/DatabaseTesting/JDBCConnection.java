2193818492d7736f129b0dad4b8f158862d4603b
alohageck0 <royalfiish@gmail.com>
2016-05-03 00:08:11 -0700

    home 05/03/16

--- src/test/java/DatabaseTesting/JDBCConnection.java
+++ src/test/java/DatabaseTesting/JDBCConnection.java
@@ -1,10 +1,29 @@
 package DatabaseTesting;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Properties;
 
 public class JDBCConnection {
    public static void main(String[] args) throws SQLException {
-      DriverManager.getConnection("jdbc:mysql://" + "localhost:3306" + "/QADBT", "root", "8wyN#7GO;XPB");
+      String host = "localhost";
+      String port = "3306";
+      String database = "QADBT";
+
+      Properties p = new Properties();
+      p.setProperty("user", "root");
+      p.setProperty("password", "8wyN#7GO;XPB");
+      p.setProperty("verifyServerCertificate", "false");
+      p.setProperty("useSSL", "false");
+      p.setProperty("serverTimezone","UTC");
+
+      Connection connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, p);
+
+      Statement s = connection.createStatement();
+      ResultSet resultSet = s.executeQuery("select * from Credentials where scenario='zerobalancecard';");
+
+      while (resultSet.next()) {
+         System.out.println(resultSet.getString("username"));
+         System.out.println(resultSet.getString("password"));
+      }
    }
 }
